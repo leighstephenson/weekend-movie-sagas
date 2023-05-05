e2b1026a63bde00a20b46869c28ebca4bdf99d38
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-05-04 23:42:23 -0500

    Finished adding description on details page and some basic MUI

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,21 +3,17 @@ import { useHistory, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Typography } from "@mui/material";
 import axios from "axios";
-import Card from "@mui/material";
-import CardContent from "@mui/material";
-import Button from "@mui/material";
+import Card from '@mui/material/Card';
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 import Grid from "@mui/material";
 
 function Details() {
 
-    let dispatch = useDispatch(); 
+    let dispatch = useDispatch();
     let history = useHistory();
     let { id } = useParams();
-    
     const selectedMovie = useSelector((store) => store.selectedMovie);
-
-
-
 
     //! Back to MovieList
     const goBack = () => { history.push('/') }
@@ -25,10 +21,30 @@ function Details() {
     //! What displays
     return (
         <>
-            <Typography>
-                Details for {selectedMovie.title}
-            </Typography>
+            <Card variant='outlined' sx={{
+                boxShadow: 10,
+                margin: 10,
 
+
+            }}>
+                <CardContent>
+                    <Typography>
+                        Details for {selectedMovie.title}
+                    </Typography>
+
+                    <Typography>
+
+                        <img src={selectedMovie.poster} />
+                        <br />
+                        {selectedMovie.description}
+
+                    </Typography>
+                </CardContent>
+            </Card>
+
+            <Button variant="outlined">
+                Go Back
+            </Button>
         </>
     );
 
