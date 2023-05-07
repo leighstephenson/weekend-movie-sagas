95b56d4f78112efbabb9328ecd847dd7dc0f699a
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-05-06 19:06:15 -0500

    Deleted excess code from index.js

--- src/index.js
+++ src/index.js
@@ -2,92 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App.js';
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
-// import logger from 'redux-logger';
-// Import saga middleware
-// import createSagaMiddleware from 'redux-saga';
-// import { takeEvery, put } from 'redux-saga/effects';
-// import axios from 'axios';
 import storeInstance from './redux/store';
 
-// // Create the rootSaga generator function
-// function* rootSaga() {
-//     yield takeEvery('FETCH_MOVIES', fetchAllMovies);
-//     yield takeEvery('FETCH_GENRES', fetchGenres);
-// }
-
-// function* fetchAllMovies() {
-//     // get all movies from the DB
-//     try {
-//         const movies = yield axios.get('/api/movie');
-//         console.log('get all:', movies.data);
-//         yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-//     } catch {
-//         console.log('get all error');
-//     }
-
-// }
-
-// function* fetchGenres(action) {
-//     // get genres from the DB 
-//     try {
-//         const movieId = action.payload;
-//         const response = yield axios.get(`/api/genre/${movieId}`);
-//         yield put({ type: 'SET_GENRES', payload: response.data });
-//     } catch (error) {
-//         console.log('Error in fetchGenres on index', error);
-//     }
-// };
-
-// // Create sagaMiddleware
-// const sagaMiddleware = createSagaMiddleware();
-
-// // Used to store movies returned from the server
-// const movies = (state = [], action) => {
-//     switch (action.type) {
-//         case 'SET_MOVIES':
-//             return action.payload;
-//         default:
-//             return state;
-//     }
-// }
-
-// // Used to store the movie genres
-// const genres = (state = [], action) => {
-//     switch (action.type) {
-//         case 'SET_GENRES':
-//             return action.payload;
-//         default:
-//             return state;
-//     }
-// }
-
-// // When users click 
-// const selectedMovie = (state = {}, action) => {
-//     if (action.type === 'SET_SELECTED_MOVIE') {
-//         return action.payload
-//     } else if (action.type === 'CLEAR_SELECTED_MOVIE') {
-//         return {}
-//     }
-//     return state
-// };
-
-// // Create one store that all components can use
-// const storeInstance = createStore(
-//     combineReducers({
-//         movies,
-//         genres,
-//         selectedMovie
-//     }),
-//     // Add sagaMiddleware to our store
-//     applyMiddleware(sagaMiddleware, logger),
-// );
-
-// // Pass rootSaga into our sagaMiddleware
-// sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
